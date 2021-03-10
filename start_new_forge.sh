052bd86f7783e5e3f1b54c12ea306f16a65d1135
e96031413 <30921855+e96031413@users.noreply.github.com>
2021-03-10 08:00:58 +0800

    Add files via upload

---
+++ start_new_forge.sh
@@ -0,0 +1,19 @@
+conda update python
+conda update conda
+conda install python=3.7
+pip install torch==1.7.0+cu92 torchvision==0.8.0+cu92 torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install -r requirements.txt
+
+sudo apt-get install cron
+sudo apt-get install xvfb
+sudo apt-get install chromium-browser
+dpkg --configure -a
+apt-get install -f
+wget https://chromedriver.storage.googleapis.com/87.0.4280.88/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip && rm chromedriver_linux64.zip
+mv chromedriver /usr/bin/
+sudo /usr/sbin/service cron start
+(crontab -l 2>/dev/null; echo "0 * * * * /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/PM25_Line_Sender.py") | crontab -
+(crontab -l 2>/dev/null; echo "0 7 * * 2,3,4,5,6 /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/us-stock.py") | crontab -
+(crontab -l 2>/dev/null; echo "0 12 * * 1-5 /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/Currency_Sender.py") | crontab -
+(crontab -l 2>/dev/null; echo "0 9 * * 1-6 /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/auto-search-flask-job-email/main.py") | crontab -
