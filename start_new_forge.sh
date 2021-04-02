52291734277bae1a1faebca5d828101408155549
e96031413 <30921855+e96031413@users.noreply.github.com>
2021-04-02 11:37:59 +0800

    Update start_new_forge.sh

--- start_new_forge.sh
+++ start_new_forge.sh
@@ -1,6 +1,4 @@
-conda update python
-conda update conda
-conda install python=3.7
+sudo apt update
 pip install torch==1.7.0+cu92 torchvision==0.8.0+cu92 torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
 pip install selenium twder
