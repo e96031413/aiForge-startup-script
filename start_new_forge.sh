adbbb55421339fc277ee30de1c636c940a6c20bb
e96031413 <30921855+e96031413@users.noreply.github.com>
2021-07-14 20:17:36 +0800

    Update start_new_forge.sh

--- start_new_forge.sh
+++ start_new_forge.sh
@@ -1,7 +1,7 @@
 sudo apt update
 pip install selenium twder beautifulsoup4
 pip install torch==1.7.1+cu92 torchvision==0.8.2+cu92 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
-pip install tensorflow-gpu==2.0.0
+# pip install tensorflow-gpu==2.0.0
 conda install -y tsnecuda cuda100 -c cannylab
 
 # grad-cam
@@ -19,7 +19,9 @@ wget https://chromedriver.storage.googleapis.com/90.0.4430.24/chromedriver_linux
 unzip chromedriver_linux64.zip && rm chromedriver_linux64.zip
 mv chromedriver /usr/bin/
 sudo /usr/sbin/service cron start
-(crontab -l 2>/dev/null; echo "0 * * * * /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/PM25_Line_Sender.py") | crontab -
+(crontab -l 2>/dev/null; echo "0 8 * * 1-6 /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/PM25_Line_Sender.py") | crontab -
+(crontab -l 2>/dev/null; echo "0 12 * * 1-6 /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/PM25_Line_Sender.py") | crontab -
+(crontab -l 2>/dev/null; echo "0 16 * * 1-6 /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/PM25_Line_Sender.py") | crontab -
 (crontab -l 2>/dev/null; echo "0 7 * * 2,3,4,5,6 /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/us-stock.py") | crontab -
 (crontab -l 2>/dev/null; echo "0 12 * * 1-5 /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/Currency_Sender.py") | crontab -
 (crontab -l 2>/dev/null; echo "0 9 * * 1-6 /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/auto-search-flask-job-email/main.py") | crontab -
