86753ea375a4231341054e5691ff31b52a3e0fec
e96031413 <30921855+e96031413@users.noreply.github.com>
2022-05-24 14:26:42 +0800

    Update start_new_forge.sh

--- start_new_forge.sh
+++ start_new_forge.sh
@@ -1,12 +1,14 @@
 sudo apt update
+pip uninstall -y torch torchvision 
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 pip install selenium twder beautifulsoup4
-pip install torch==1.7.1+cu92 torchvision==0.8.2+cu92 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html  #目前aiForge環境(CUDA=10.0)最高只支援到1.7.1版本
 
-pip install tensorboard==1.15
+pip install tensorboard==2.9.0
 pip install ../tsnecuda-2.1.0-py2.py3-none-any.whl
-# pip install tensorflow-gpu==2.0.0
+pip uninstall -y tensorflow && pip install tensorflow
 pip install timm==0.3.2 tensorboardX six
 pip install pyro-ppl==1.5.1
+pip install thop tqdm
 
 # grad-cam
 pip uninstall -y enum34
@@ -32,4 +34,4 @@ sudo /usr/sbin/service cron start
 (crontab -l 2>/dev/null; echo "0 12 * * 0-6 /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/Currency_Sender.py") | crontab -
 (crontab -l 2>/dev/null; echo "0 9 * * 0-6 /opt/conda/bin/python3.6 /root/notebooks/nfs/work/yanwei.liu/WebCrawler/auto-search-flask-job-email/main.py") | crontab -
 
-conda create --name pacl python=3.6 && conda activate pacl && pip install seaborn tensorboardX crc32c && pip install -U scikit-learn && pip install torchnet thop==0.0.4-2006032126 && pip install openpyxl && pip install torch==1.7.1+cu92 torchvision==0.8.2+cu92 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html && conda deactivate
+# conda create --name pacl python=3.6 && conda activate pacl && pip install seaborn tensorboardX crc32c && pip install -U scikit-learn && pip install torchnet thop==0.0.4-2006032126 && pip install openpyxl && pip install torch==1.7.1+cu92 torchvision==0.8.2+cu92 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html && conda deactivate
